53f0430950e464c2051583998118074d4afc1a24
mshulezhko <119418285+mshulezhko@users.noreply.github.com>
2024-01-06 15:39:09 +0100

    add new project

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -3,11 +3,37 @@ import Project from "./Project";
 import WeatherImg from './../../images/weather.png'
 import NetworkImg from './../../images/network.png'
 import Countries from './../../images/countries.png'
+import Game from './../../images/game.png'
 import './../../styles/projects.css'
 
 
 function Projects() {
     return <section id="projects">
+                 <Project
+            title='social network WITH COLOR THEME SWITCHER'
+            description='In this project I used
+                    REST APIs (social-network.samuraijs.com), React, JavaScript, Css, HTML, redux  to create a responsive web application.
+                    You need to enter these data to log in. Login: mariyaschulezhko@gmail.com
+password: free1   (login in the anonymous browsing window
+does not work)'
+            git="https://github.com/mshulezhko/portfolio"
+            liveDemo="https://graceful-sopapillas-8c6664.netlify.app/#/login"
+            img={NetworkImg}
+
+        />
+
+
+                 <Project
+            title='Rock, Paper, Scissors game'
+            description="In my project showcasing the classic game of Rock, Paper, Scissors, I utilized a technology stack comprising JavaScript, CSS, HTML, and React as well as Responsive Design. The game provides an interactive and dynamic user experience, taking advantage of React's component-based architecture."
+            git="https://github.com/mshulezhko/rock-paper-scissors"
+            liveDemo="https://courageous-lollipop-acf937.netlify.app"
+            img={Game}
+             resource='https://www.frontendmentor.io/'
+            resourceLink='https://www.frontendmentor.io/challenges/rock-paper-scissors-game-pTgwgvgH'
+
+        />
+
                  <Project
             title='REST Countries API with color theme switcher'
             description='In this project, I utilized REST APIs (https://restcountries.com/), React, JavaScript, CSS, and HTML to create a responsive web application. The application features a country search form that enables users to search for countries by name and a filter by region functionality and it includes an implementation of a color theme switcher with toggle functionality between light and dark themes.'
@@ -21,7 +47,8 @@ function Projects() {
 
         />
 
-        <Project
+
+               <Project
             title='Weather Forecast'
             description='In this project I used
                     REST APIs (api.shecodes.io), React, JavaScript, Css, HTML to create a responsive web application'
@@ -30,19 +57,6 @@ function Projects() {
             img={WeatherImg}
 
         />
-         <Project
-            title='social network WITH COLOR THEME SWITCHER'
-            description='In this project I used
-                    REST APIs (social-network.samuraijs.com), React, JavaScript, Css, HTML, redux  to create a responsive web application.
-                    You need to enter these data to log in. Login: mariyaschulezhko@gmail.com
-password: free1   (login in the anonymous browsing window
-does not work)'
-            git="https://github.com/mshulezhko/portfolio"
-            liveDemo="https://graceful-sopapillas-8c6664.netlify.app/#/login"
-            img={NetworkImg}
-
-        />
-       
     </section>
 }
 
